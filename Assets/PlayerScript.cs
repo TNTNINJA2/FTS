d4f826b0b36e9beeb2346420ea46d4d6ce0e3d6e
Bazelsneeze <109120259+Bazelsneeze@users.noreply.github.com>
2022-12-31 18:14:49 -0800

    Added Respawn Function, and checkpoint prefab
    respawn function is public, currently runs off a keybind for resetting to checkpoint that is bound for "l"

--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+
 public class PlayerScript : MonoBehaviour
 {
     [SerializeField]
@@ -35,6 +36,11 @@ public class PlayerScript : MonoBehaviour
     private InputAction jumpAndDiveControls;
     [SerializeField]
     private InputAction dashControls;
+    [SerializeField]
+    private InputAction respawnControls;
+
+    [SerializeField]
+    private GameObject lastCheckpoint;
 
 
     private bool canDash = true;
@@ -54,6 +60,14 @@ public class PlayerScript : MonoBehaviour
             if (!IsOnGround())
             {
                 rigidBody2D.velocity = new Vector2(rigidBody2D.velocity.x, rigidBody2D.velocity.y - diveSpeed);
+            }
+        }
+
+        if (respawnControls.ReadValue<float>() == 1)
+        {
+            if (lastCheckpoint != null)
+            {
+                returnToCheckpoint();
             }
         }
 
@@ -78,6 +92,7 @@ public class PlayerScript : MonoBehaviour
             }
         }
 
+      
        
         rigidBody2D.velocity = new Vector2(rigidBody2D.velocity.x + movementControls.ReadValue<float>() * moveSpeed * Time.deltaTime, rigidBody2D.velocity.y);
         if (shouldClampHoriznontalSpeed)
@@ -91,6 +106,7 @@ public class PlayerScript : MonoBehaviour
         movementControls.Enable();
         jumpAndDiveControls.Enable();
         dashControls.Enable();
+        respawnControls.Enable();
     }
 
     private void OnDisable()
@@ -98,6 +114,7 @@ public class PlayerScript : MonoBehaviour
         movementControls.Disable();
         jumpAndDiveControls.Disable();
         dashControls.Disable();
+        respawnControls.Disable();
     }
 
     private void OnCollisionStay2D(Collision2D collision)
@@ -116,18 +133,36 @@ public class PlayerScript : MonoBehaviour
             {
                 rigidBody2D.velocity = new Vector2(jumpVelocity, jumpVelocity);
             }
+
+        }
+        if (collision.collider.gameObject.tag.Equals("Checkpoint"))
+        {
+            lastCheckpoint = collision.collider.gameObject; 
         }
     }
 
     public bool IsOnGround()
     {
-        GameObject[] groundObjects = GameObject.FindGameObjectsWithTag("Ground");
-        foreach (GameObject groundObject in groundObjects)
-        {
-            if (bottomEdgeCollider.IsTouching(groundObject.GetComponent<Collider2D>())) {
-                return true;
+        Component[] colliders = Component.FindObjectsOfType<Collider2D>();
+        
+        foreach (Component collider in colliders)
+        {if (!bottomEdgeCollider.Equals(collider))
+            {
+                if (bottomEdgeCollider.IsTouching(collider.GetComponent<Collider2D>()))
+                {
+                    
+                    return true;
+                }
             }
         }
+      
         return false;
+    }
+
+    public void returnToCheckpoint()
+    {
+        rigidBody2D.velocity = Vector2.zero;
+        transform.position = lastCheckpoint.transform.position + new Vector3(0, 0.45f, 0);
+        Debug.Log("trying to respawn");
     }
 }
